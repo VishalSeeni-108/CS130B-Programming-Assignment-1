e1af2e88c2c935b545e04c42f4086b821f137468
Vishal S <vishalseeni@gmail.com>
2024-04-30 12:52:21 -0700

    First attempt, passing test case 1

--- Watering Grass.cpp
+++ Watering Grass.cpp
@@ -2,8 +2,23 @@
 #include <string>
 #include <sstream> 
 #include <vector>
+#include <math.h>
+#include <algorithm> 
+#include <float.h>
 
 using namespace std; 
+
+struct sprinkler
+{
+    long double position; 
+    long double radius; 
+};
+
+struct coverageRectangle
+{
+    long double start; 
+    long double end; 
+}; 
 
 main()
 {
@@ -12,38 +27,121 @@ main()
     getline(cin, input);
     while(input != "")
     {
-        int numSprinklers; 
-        int length; 
-        int width; 
+        long double numSprinklers; 
+        long double length; 
+        long double width; 
         stringstream caseInput(input); 
         caseInput >> numSprinklers; 
         caseInput >> length; 
         caseInput >> width; 
 
-        vector<vector<int>> sprinklers; 
+        vector<sprinkler> sprinklers; 
         for(int i = 0; i < numSprinklers; i++)
         {
-            int position; 
-            int radius; 
+            long double position; 
+            long double radius; 
             cin >> position;
             cin >> radius; 
 
-            vector<int> newSprinkler; 
-            newSprinkler.push_back(position); 
-            newSprinkler.push_back(radius); 
+            sprinkler newSprinkler; 
+            newSprinkler.position = position; 
+            newSprinkler.radius = radius; 
             sprinklers.push_back(newSprinkler); 
         }
 
-        cout << numSprinklers << " " << length << " " << width << endl; 
+        // cout << numSprinklers << " " << length << " " << width << endl; 
+        // for(int i = 0; i < numSprinklers; i++)
+        // {
+        //     cout << sprinklers.at(i).position << " " << sprinklers.at(i).radius << endl; 
+        // }
+
+        //IDEA: Each circle only fully covers a rectangle of height w - any portion that is only partially covered will need another circle to cover it, 
+        // and since all sprinklers are along the same axis, it will have to cover it by its own rectangle in order to fully cover it. Thus, only the rectangles of full coverage are 
+        //relevant. 
+        // After reading in input, translate circles into covered rectangles (note that any circles smaller than the width of the grass will have no fully covered rectangles, and
+        // will be left out).
+        // With the rectagles, we can use our previous interval algorithm. 
+
+        
+        //Convert vector of circles into vectors of rectangles
+        vector<coverageRectangle> rectangles; 
         for(int i = 0; i < numSprinklers; i++)
         {
-            cout << sprinklers.at(i).at(0) << " " << sprinklers.at(i).at(1) << endl; 
+            //Check if circle is at least as large as the width of the grass
+            if(sprinklers.at(i).radius >= width)
+            {
+                //Calculate x, the distance from the center of the circle to the left/right sides of the rectangle
+                long double x = sqrt(pow(sprinklers.at(i).radius, 2) - pow((width / 2), 2)); 
+
+                //Store the start of the rectange (sprinkler position - x) and the end (sprinkler postion + x)
+                coverageRectangle newRectangle; 
+                newRectangle.start = (sprinklers.at(i).position - x); 
+                newRectangle.end = (sprinklers.at(i).position + x); 
+
+                rectangles.push_back(newRectangle); 
+            }
         }
 
-        cout << "END CASE" << endl; 
-        cout << endl; 
+        //Perform interval choosing algorithm on rectangles
+        //First, sort the vector 
+        vector<coverageRectangle> sortedRectangles; 
+        for(int i = 0; i < rectangles.size(); i++)
+        {
+            int indexOfEarliest = 0;
+            long double earliestStart = DBL_MAX; 
+            for(int j = 0; j < rectangles.size(); j++)
+            {
+                if(rectangles.at(j).start < earliestStart)
+                {
+                    earliestStart = rectangles.at(j).start; 
+                    indexOfEarliest = j; 
+                }
+            }
 
+            coverageRectangle copyRectangle; 
+            copyRectangle.start = rectangles.at(indexOfEarliest).start; 
+            copyRectangle.end = rectangles.at(indexOfEarliest).end; 
+            sortedRectangles.push_back(copyRectangle); 
+            rectangles.at(indexOfEarliest).start = DBL_MAX; 
+        }
+
+        //Next, starting from position 0, choose intervals such that they start before the current position and extend as far after it as possible
+        long double currentCovered = 0; 
+        int numIntervals = 0; 
+
+        while(currentCovered < length)
+        {
+            long double nextIntervalEnd = -(DBL_MAX); 
+            bool validIntervalFound = false; 
+            for(int i = 0; i < sortedRectangles.size(); i++)
+            {
+                if(sortedRectangles.at(i).start < currentCovered)
+                {
+                    if(sortedRectangles.at(i).end > nextIntervalEnd)
+                    {
+                        nextIntervalEnd = sortedRectangles.at(i).end; 
+                        validIntervalFound = true; 
+                    }
+                }
+            }
+        
+            if(!validIntervalFound)
+            {
+                numIntervals = -1; 
+                break; 
+            }
+            else
+            {
+                currentCovered = nextIntervalEnd; 
+                numIntervals++; 
+            }
+        }
+    
+        cout << numIntervals << endl; 
         getline(cin, input);
         getline(cin, input); 
     }
+
+
+    
 }
